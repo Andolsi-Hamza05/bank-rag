866aec2b93d3648ebee664f8549fd2d1bcd716ea
Andolsi-Hamza05 <hamza.landolsi@essai.ucar.tn>
2024-07-06 18:22:57 +0100

    Editing the function get_prompt_template in utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
-def get_prompt_tempalte():
-    template_file = "prompts/naive_rag.txt"
-    with open(template_file, "r", encoding="utf-8") as file:
+def get_prompt_template(template_path: str):
+    with open(template_path, "r", encoding="utf-8") as file:
         template_text = file.read()
     return template_text
